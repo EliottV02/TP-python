854a62d885e5687fd11b729b34f06db016c648a2
eliott vainchtock <81221438+EliottV02@users.noreply.github.com>
2023-01-30 16:49:20 +0100

    class humain

---
+++ Humain.py
@@ -0,0 +1,15 @@
+
+class Humain():
+    def __init__(self, nom, prenom):
+        self.nom = nom
+        self.prenom = prenom
+
+    def marcherHumain(self, x, y):
+        print("l'humain se deplace de", x, "à", y)
+
+    def OrdreHumain(self, ordres):
+        print("Quel ordre voulez vous donner ?")
+        # robot.ramene_une_chaussette
+
+    def superviserRobot(self):
+        print("est-ce que le robot a bien fait sont travail ?")
